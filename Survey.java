3899d64c1ed1d07ffec5dc3acabedabc28a11143
TobiasButler <43075586+TobiasButler@users.noreply.github.com>
2018-11-10 01:41:14 -0800

    Add files via upload

--- Survey.java
+++ Survey.java
@@ -1,63 +1,70 @@
 public class Survey{
+    
     //instance variables
     private String userName;
-    private String name;
-    private int exp;
-    private int BenchWt;
     private Routine previousRoutine;
     private Routine newRoutine;
+    private int exp;
+    private int focus;
+    private int SqtMx;
+    private int BnchMx;
+    private int ClnMx;
     
-
     //default constructor
     public Survey(){
 
-}
-    public Survey(String userName, String reportName){
-
-
+    }
+    //primary constructor
+    public Survey(User user){
+        this.previousRoutine = user.getRoutine();
     }
 
     // displays the Survey to the user
     public void displaySurvey(){
-
-
+        //GUI WORK
     }
     // exits the Survey, hiding it from the user
     public void closeSurvey(){
-
-
+        //GUI WORK
     }
     // hides the Survey from the user. Attempts to save data.
     public void hideSurvey(){
+        //GUI WORK
+    }
 
-
+    public void getExperience(){
+        
+    }
+    public void getfocus(){
+        
+    }
+    public void getSqtMx(){
+        
+    }
+    public void getBnchMx(){
+        
+    }
+    public void getClnMx(){
+        
+    }
+    public void sendData(){
+        // sends stored data to the database
     }
     // acquires the user's current routine
-    public void getRoutine(String userName){
-
-
+    public Routine generateRoutine(Routine previousRoutine){
+        // edit Routine
+        return previousRoutine;
     }
     //
-    public selectExercisesCompleted(){
-
-
+    public void selectExercisesCompleted(){
+        //GUIWORK
     }
     // sets the user's routine as the newly constructed one
-    public void setRoutine(){
-
-    
+    public void setRoutine(Routine newRoutine){
+        this.newRoutine = newRoutine;
     }
-    // 
-    public void getExp(){
 
-
-    }
-    public void setExp(){
-
-
-    }
     public void setUserComfort(int comfort){
-
 
     }
     public String toString(){
